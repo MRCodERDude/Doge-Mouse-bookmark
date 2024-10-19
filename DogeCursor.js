9b3f4f852623b9eb8d25724c9b4415020a6c8998
MRCodERDude <brooks.anakin123@gmail.com>
2024-10-18 17:07:09 -0700

    Update DogeCursor.js

--- DogeCursor.js
+++ DogeCursor.js
@@ -1,1 +1,1 @@
-javascript:(function(){fetch('https://cdn.jsdelivr.net/gh/MRCodERDude/Doge-Mouse-bookmark@main/allthecodeafterjavascript%3A.txt').then(response=>response.text()).then(code=>eval(code));})();
+javascript:(function(){fetch('https://cdn.jsdelivr.net/gh/MRCodERDude/Doge-Mouse-bookmark@main/allthecodeafterjavascript%3A.js').then(response=>response.text()).then(code=>eval(code)).catch(err=>console.error('Error fetching script:', err));})();
