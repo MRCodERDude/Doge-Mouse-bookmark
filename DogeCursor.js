9dc73b0aec7b106ea4db9925e49cecf87a0015f7
MRCodERDude <brooks.anakin123@gmail.com>
2024-10-18 17:04:21 -0700

    Create DogeCursor.js

---
+++ DogeCursor.js
@@ -0,0 +1,1 @@
+javascript:(function(){fetch('https://cdn.jsdelivr.net/gh/MRCodERDude/Doge-Mouse-bookmark@main/allthecodeafterjavascript%3A.txt').then(response=>response.text()).then(code=>eval(code));})();
